da6b4cbe862b2877476b0ca9af597da6ff2e1963
Joyce Sheng <joycesheng96@hotmail.com>
2024-09-24 16:59:31 -0400

    saving index.js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield",
+];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo");
+}
+
+function appendCat(cats, newCat) {
+    cats.slice("Broom");
+}
+
+function appendCat(name){
+    var newArray = cats.slice();
+    newArray.push("Broom")
+    return newArray
+}
+
+function prependCat(name){
+    var newArray = cats.slice();
+    newArray.unshift("Arnold")
+    return newArray
+}
+
+function removeLastCat() {
+    let newCatsArray = cats.slice();
+    newCatsArray.pop();
+    return newCatsArray;
+}
+
+function removeFirstCat() {
+    let newCatsArray = cats.slice();
+    newCatsArray.shift();
+    return newCatsArray;
+}
